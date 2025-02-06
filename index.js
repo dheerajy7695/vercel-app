3165f472b814b0b5ee15dec937eb3ff593f9209e
dheerajy7695 <dheerajy906@gmail.com>
2025-02-06 19:18:13 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    res.send("Express on Vercel");
+});
+
+app.get("/home", (req, res) => {
+    res.send("Welcome to your home page");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+
+module.exports = app;
